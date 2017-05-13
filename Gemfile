024c52770a4da3c08a9b52e3b2afe2a15d53437e
mmendesc <matheus.mendescf@gmail.com>
2017-05-13 11:12:54 -0300

    Heroku

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
 # Use postgresql as the database for Active Record
@@ -62,3 +63,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+ruby "2.4.0"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,23 @@
 # server-based syntax
-set :puma_env, :production
-set :rack_env, :production
-server '54.202.231.125', user: 'deploy', roles: %w{web app db}
+
+
+# set :puma_env, :production
+# set :rack_env, :production
+# server '54.202.231.125', user: 'deploy', roles: %w{web app db}
+
+
+
+
+
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+if ENV["RAILS_LOG_TO_STDOUT"].present?
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+end
+
+
+
 
 
 # role-based syntax
