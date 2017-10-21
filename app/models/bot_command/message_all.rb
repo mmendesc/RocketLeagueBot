18ded7d1722b66944be067e6e109e5ead3d08195
mmendesc <matheus.mendescf@gmail.com>
2017-10-21 16:12:04 -0300

    Cludge fix

--- app/models/bot_command/message_all.rb
+++ app/models/bot_command/message_all.rb
@@ -2,6 +2,12 @@ module BotCommand
   class MessageAll < Base
 
     def message_all
+      # @user.set_next_bot_command('BotCommand::MessageAll','send_to_all')
+      # @user.save
+      # send_message 'Type the message to send to all.'
+    end
+
+    def send_to_all
       @message.slice!("/#{Figaro.env.message_all} ")
       @message.slice!("/#{Figaro.env.message_all}")
       User.where(found: true).each do |user|
